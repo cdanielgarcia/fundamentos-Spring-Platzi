300e315c17828c676a42b400e8bc49d039ab06f9
Daniel Garcia <danielgarciaabello@gmail.com>
2022-03-23 23:23:43 -0500

    first commit

---
+++ src/main/java/com/fundamentosnuevos/springboot/fundamentosNuevos/FundamentosNuevosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosnuevos.springboot.fundamentosNuevos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosNuevosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosNuevosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosnuevos/springboot/fundamentosNuevos/FundamentosNuevosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosnuevos.springboot.fundamentosNuevos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosNuevosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
